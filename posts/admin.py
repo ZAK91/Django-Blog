0f327309300c8f0cd156dafcc278efd50ea2f541
ZAK91 <120594345+ZAK91@users.noreply.github.com>
2023-05-16 18:20:27 +0200

    customize admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,12 @@ from django.contrib import admin
 
 # Register your models here.
 from .models import post ,Author
+
+class PostAdmin(admin.ModelAdmin):
+    list_display=['title' ,'author','publish_date']
+    list_filter=['author','tags','publish_date']
+    search_fields= ['title','content']
+
+
 admin.site.register(post)
 admin.site.register(Author)
